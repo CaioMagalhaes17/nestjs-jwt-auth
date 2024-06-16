3cd96dc13a3cb28ca5c587ef6db78525c5108841
unknown <caiomagalhaesdefaria@hotmail.com>
2024-06-16 19:06:50 -0300

    Ajustes finais no .env

--- src/database/sequelize.config.service.ts
+++ src/database/sequelize.config.service.ts
@@ -1,12 +1,14 @@
 import { SequelizeModule } from "@nestjs/sequelize";
 import { User } from "./models/user.model";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const SequelizeConfigService = SequelizeModule.forRoot({
   dialect: 'mysql',
-  host: '127.0.0.1',
-  port: 3306,
-  username: 'root',
-  password: '01052003Cc@',
-  database: 'jwt_auth',
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USER_NAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE_NAME,
   models: [User],
 })
